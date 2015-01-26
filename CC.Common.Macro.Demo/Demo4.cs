33cf657d32699327011546fa1d6e5f41f5d436c1
Ken Wilcox <wilcoxk@gmail.com>
2015-01-26 14:47:13 -0700

    another class for the demo macro

---
+++ CC.Common.Macro.Demo/Demo4.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CC.Common.Macro.Demo
+{
+  class Demo4
+  {
+    public string Name
+    {
+      get { return "John Que Public"; }
+    }
+
+    public string Hobby
+    {
+      get { return "Programming"; }
+    }
+
+    public DateTime Birthday
+    {
+      get { return DateTime.Today; }
+    }
+
+    public int FavoriteNumber
+    {
+      get { return 42; }
+    }
+
+    public bool IsHappy
+    {
+      get { return true; }
+    }
+
+    [System.ComponentModel.Browsable(false)]
+    public string YouCanNotSeeThis
+    {
+      get { return "Since this is not browsable, you can't see me"; }
+    }
+  }
+}
